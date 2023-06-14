90f26481f511753cf46e9f56cff8df182ec3238f
blueceo75 <52962645+blueceo75@users.noreply.github.com>
2023-06-15 01:42:58 +0900

    Add files via upload

---
+++ test.java
@@ -0,0 +1,13 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Hello world!\n");
+		System.out.print("안녕하세요");
+		System.out.println("반가워요");
+		System.out.println("hi");
+	}
+
+}
